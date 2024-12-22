c4a58786ff696f4de9c202a4bc9450cf83d3edc2
Gunjan <glunkad26@gmail.com>
2024-12-22 22:58:59 +0530

    done almost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,1 @@
 export const API = "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct/v1/chat/completions";
-export const TOKEN = "Bearer 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";
