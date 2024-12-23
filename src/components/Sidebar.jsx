eb04f39c654d73d39158eb0536d085a4d52346b7
Gunjan <gunjan.lunkad@medimaze.in>
2024-12-23 15:21:38 +0530

    style: added new icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,10 +7,13 @@ const Sidebar = () => {
 
     return (
         <aside className="bg-neutral-800 w-64 shrink-0 p-3 ">
-            <div className="">
-                <img src="https://www.chatpdf.com/_next/static/media/SidebarLogo.243d2948.svg"  alt="Care Simplify"/>
+            <div className="flex">
+                <img src="https://www.chatpdf.com/_next/static/media/SidebarLogo.243d2948.svg" alt="Care Simplify"/>
+                <img src="../assets/svgs/sidebar.svg" alt="sidebar"/>
             </div>
-            <button className="bg-green-600 text-white text-base py-2 border rounded-md border-transparent w-full hover:bg-green-800 my-4" onClick={handleOnClick}>
+            <button
+                className="bg-green-600 text-white text-base py-2 border rounded-md border-transparent w-full hover:bg-green-800 my-4"
+                onClick={handleOnClick}>
                 <span className="mr-1">+</span>
                 New Chat
             </button>
