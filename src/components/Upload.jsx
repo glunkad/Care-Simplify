47785c822927883b50461691631965da756d0cbb
Gunjan <gunjan.lunkad@medimaze.in>
2025-03-12 11:55:52 +0530

    style: fixed the upload container

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -6,7 +6,7 @@ const Upload = () => {
     const [fileName, setFileName] = useState("");
     const [isUploading, setIsUploading] = useState(false);
 
-    const {setSharedFile } = useSharedFile();
+    const { setSharedFile } = useSharedFile();
 
     const handleButtonClick = () => {
         const fileInput = document.getElementById("fileInput");
@@ -21,26 +21,23 @@ const Upload = () => {
                 setSharedFile(selectedFile);
             } else {
                 alert("Only PDF files are allowed.");
-                event.target.value = ""; // Reset the file input
+                event.target.value = "";
                 setFileName("");
             }
         }
     };
 
-
     return (
-        // <div className="bg-gray-100 w-full p-0 flex justify-center items-center flex-col">
         <div className="bg-gradient-to-r from-indigo-100 from-20% via-sky-100 via-30% to-emerald-100 to-100% w-full flex flex-col justify-center items-center ">
             <div>
-                <h1 className="text-5xl my-2.5">Chat with any Clinical Report</h1>
+                <h1 className="text-5xl my-2.5">Chat with any PDF</h1>
             </div>
-            <div className="border-8 p-6 rounded-2xl bg-white border-transparent shrink-0 shadow-2xl shadow-green-500 sm: p-2">
-                {/* Clickable container */}
+            <div className="border-8 rounded-2xl bg-white border-transparent shrink-0 shadow-2xl shadow-green-500 sm:p-4">
                 <label
                     htmlFor="fileInput"
-                    className="px-52 py-16 flex flex-col justify-center items-center cursor-pointer text-center border-2 rounded-xl border-dashed border-green-600 hover:bg-green-100 sm: px-36  py-12"
+                    className="flex flex-col justify-center items-center cursor-pointer text-center border-2 rounded-xl border-dashed border-green-600 hover:bg-green-100
+                    p-12 sm:p-14 md:p-16 lg:p-20 w-72 sm:w-96 md:w-[26rem] lg:w-[42rem]"
                 >
-                    {/* Hidden file input */}
                     <input
                         type="file"
                         className="hidden"
@@ -56,7 +53,6 @@ const Upload = () => {
                     <div>
                         <span>Click to upload, or drag PDF here</span>
                     </div>
-                    {/* Upload button */}
                     <div className="my-5">
                         <button className="cursor-pointer overflow-hidden items-center bg-green-600 inline-flex justify-center text-white flex-row h-12 rounded-lg hover:bg-green-800" onClick={handleButtonClick}>
                             <span className="flex px-6 items-center">
@@ -74,6 +70,5 @@ const Upload = () => {
         </div>
     );
 };
-
 
 export default Upload;
